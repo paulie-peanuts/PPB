a3950c3a6d74017e2a130ad88187fa7ca4d0dc7b
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-06-12 10:28:01 -0500

    data added

--- print.r
+++ print.r
@@ -3,20 +3,21 @@ print(2+2)
 # title: "Introduction to Data Frames in R"
 # output: html_notebook
 # ---
-
+# ```{r}
 # ```{r message=FALSE, warning=FALSE}
 # # load libraries
 # library(haven)
 # library(readr)
-# library(dplyr)
+library(dplyr)
 # ```
 
 # ```{r}
 # # load data frame
 songs <- read.table("Jimle.dat")
-print(songs)
+print(head(select(songs, V1, V2)))
+# for song in songs
+# if (songs[songs[,1]])
 # ```
-
+print(filter(songs, V1 == 14001))
 # ```{r}
-# print(songs)
 # ```
